376ed1111b4534ef699d00e1230066c047ad3e9c
SKor11 <sergeirobota@gmail.com>
2016-11-08 17:39:26 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        int a = 126;
+        int b = a % 2;
+
+        String c = (b == 0)? "Yes" : "No" ;
+
+        System.out.print(c);
+    }
+}
